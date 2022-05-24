65118b65bf48580a0d44e09af7638d19bb4aef61
Lavs-GIT <lavanya.rajalingam@gmail.com>
2022-05-24 17:13:15 +0530

    Updated API URL

--- guestbook.js
+++ guestbook.js
@@ -7,7 +7,7 @@ const guestbook = {
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `https://e30a52ca.eu-gb.apigw.appdomain.cloud/guestbook/entries`,
       dataType: 'json'
     });
   },
